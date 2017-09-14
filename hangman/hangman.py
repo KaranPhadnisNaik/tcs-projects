ec4ccc9483ddfbe4f7e6e017b886e8c3262ccaa7
karanphadnisnaik <Karanphadnis1@gmail.com>
2017-09-14 13:25:12 -0700

    clean up hangman

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -5,77 +5,81 @@ FILE = 'words-corpus.txt'
 
 class Hangman():
     def __init__(self):
-        print "Welcome to 'Hangman', are you ready to die?"
-        print "(1)Yes, for I am already dead.\n(2)No, get me outta here!"
+        print "Welcome to Hangman!"
+        print "(1) Play?\n(2) Exit"
         user_choice_1 = raw_input("->")
 
         if user_choice_1 == '1':
-            print "Loading nooses, murderers, rapists, thiefs, lunatics..."
+            print "Starting up game...\n"
             self.start_game()
         elif user_choice_1 == '2':
-            print "Bye bye now..."
+            print "Goodbye.\n"
             exit()
         else:
-            print "I'm sorry, I'm hard of hearing, could you repeat that?"
+            print "Sorry please input either 1 or 2 as an option."
             self.__init__()
 
     def start_game(self):
-        print "Guess the right words and you can live to see another day."
-        print "But don't get so happy yet."
-        print "If you make 6 wrong guess, YOU'RE TOAST! VAMANOS!"
-        self.core_game()
+        print "========= RULES ========="
+        print "A random word will be generated that you need to guess."
+        print "Guess one letter at a time."
+        print "If you make 6 wrong guesses, you lose!"
+        print "=========================\n"
+        self.play()
         return
 
     def get_random_line(self):
-        total_bytes = os.stat(FILE).st_size
-        random_point = random.randint(0, total_bytes)
-        file = open(FILE)
-        file.seek(random_point)
-        file.readline() # skip this line to clear the partial line
-        return file.readline()
+        with open(FILE) as f:
+            line = random.sample(f.readlines(),1)
+            return line[0][:-1]
 
-    def core_game(self):
+    def play(self):
         guesses = 0
-        letters_used = ""
-        the_word = self.get_random_line()
-        #print(the_word)
-        progress = ["?" for letter in the_word]
+        used = ""
+        final = ""
+
+        word = self.get_random_line()
+        progress = ["?" for letter in word]
         # remove EOF character
         progress = progress[1:]
-        final = ""
         while guesses < 6:
-            if final == the_word[:-1]:
+            if final == word[:-1]:
                 print "==============================="
                 print "==                           =="
                 print "==         YOU WIN!          =="
                 print "==                           =="
                 print "==============================="
                 return
-            guess = raw_input("Guess a letter ->")
-            if guess in the_word and guess not in letters_used:
-                print "As it turns out, your guess was RIGHT!"
-                letters_used += "," + guess if letters_used!="" else guess
-                self.hangman_graphic(guesses)
-                final = self.progress_updater(guess, the_word, progress)
+            print ""
+            guess = raw_input("Guess a letter -> ").lower()
+
+            if guess in word and guess not in used:
+                print "Your guess was RIGHT!"
+                used += "," + guess if used!="" else guess
+                self.hangman_graphic(guesses,word)
+                final = self.progress_updater(guess, word, progress)
                 print "Progress: " + final
-                print "Letter used: " + letters_used
-            elif guess not in the_word and guess not in letters_used:
+                print "Letter used: " + used
+
+            elif guess not in word and guess not in used:
                 if len(guess) > 1:
-                    print "Please enter a single character!"
+                    print "Please enter a single letter! No numbers or punctuation allowed either!"
                     continue
+                if not guess.isalpha():
+                    print "Please enter a single letter! No numbers or punctuation allowed either!"
+                    continue
+
                 guesses += 1
-                print "Things aren't looking so good, that guess was WRONG!"
-                print "Oh man, that crowd is getting happy, I thought you"
-                print "wanted to make them mad?"
-                letters_used += "," + guess if letters_used!="" else guess
-                self.hangman_graphic(guesses)
+                print "Well? That guess was WRONG!"
+                used += "," + guess if used!="" else guess
+                self.hangman_graphic(guesses, word)
                 print "Progress: " + "".join(progress)
-                print "Letter used: " + letters_used
+                print "Letter used: " + used
             else:
-                print "That's the wrong letter, you wanna be out here all day?"
+                print "That's the wrong letter!"
                 print "Try again!"
 
-    def hangman_graphic(self, guesses):
+    def hangman_graphic(self, guesses, word):
         if guesses == 0:
             print "________      "
             print "|      |      "
@@ -86,54 +90,54 @@ class Hangman():
         elif guesses == 1:
             print "________      "
             print "|      |      "
-            print "|      0      "
+            print "|      O      "
             print "|             "
             print "|             "
             print "|             "
         elif guesses == 2:
             print "________      "
             print "|      |      "
-            print "|      0      "
-            print "|     /       "
+            print "|      O      "
+            print "|       \     "
             print "|             "
             print "|             "
         elif guesses == 3:
             print "________      "
             print "|      |      "
-            print "|      0      "
-            print "|     /|      "
+            print "|      O      "
+            print "|      |\     "
             print "|             "
             print "|             "
         elif guesses == 4:
             print "________      "
             print "|      |      "
-            print "|      0      "
+            print "|      O      "
             print "|     /|\     "
             print "|             "
             print "|             "
         elif guesses == 5:
             print "________      "
             print "|      |      "
-            print "|      0      "
+            print "|      O      "
             print "|     /|\     "
-            print "|     /       "
+            print "|       \     "
             print "|             "
         else:
             print "________      "
             print "|      |      "
-            print "|      0      "
+            print "|      O      "
             print "|     /|\     "
             print "|     / \     "
             print "|             "
-            print "The noose tightens around your neck, and you feel the"
-            print "sudden urge to urinate."
+            print "Well it looks like you LOST"
+            print "The correct word was "+word.upper()
             print "GAME OVER!"
             self.__init__()
 
-    def progress_updater(self, guess, the_word, progress):
+    def progress_updater(self, guess, word, progress):
         i = 0
-        while i < len(the_word):
-            if guess == the_word[i]:
+        while i < len(word):
+            if guess == word[i]:
                 progress[i] = guess
                 i += 1
             else:
